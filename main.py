2cc071544647d79c5a29dedaafba9aea2ef246f6
Etcetera <pietroricardocres@proton.me>
2023-05-15 08:37:10 -0300

    refactor

--- main.py
+++ main.py
@@ -17,57 +17,66 @@ class LowSpeed(Exception):
     pass
 
 
-def show_toast(msg):
-    notification = ToastNotifier()
+def notify(msg):
+    playsound('./sound.mp3')
 
+    notification = ToastNotifier()
     notification.show_toast(
         'Internet baixa', msg, duration=10)
 
 
-try:
+def validate():
     if len(sys.argv) < 3:
         raise MissingArgs
 
-    min_download = float(sys.argv[1])
-    min_upload = float(sys.argv[2])
 
+def speedtest():
     process = Popen('speedtest --format json --progress no',
                     shell=True, stdout=PIPE, stderr=PIPE)  # Executar comando CMD
 
     out, err = process.communicate()  # Receber resposta
     errcode = process.returncode  # Receber erro
 
-    speedtest = json.loads(out)  # Transformar em JSON
+    return out, err, errcode
 
-    download_speed = speedtest['download']['bandwidth'] * \
-        8 / 1_000_000  # Cálculo para transformar em Mbps
-    upload_speed = speedtest['upload']['bandwidth'] * 8 / 1_000_000
-    ping = speedtest['ping']['latency']
 
-    # DateISO to Human
-    dateISO = speedtest['timestamp']
+def ISOtoHuman(dateISO):
     dt_obj = datetime.fromisoformat(dateISO.replace('Z', '+00:00'))
     timestamp = dt_obj.timestamp()
-    date = datetime.fromtimestamp(timestamp)
-    ####
+    return datetime.fromtimestamp(timestamp)
 
-    print(f'Download: {download_speed:.2f}\nUpload: {upload_speed:.2f}')
 
-    if download_speed < min_download and upload_speed < min_upload:
-        playsound('./sound.mp3')
-        show_toast('Velocidade de download e upload estão abaixo do esperado.')
-    elif download_speed < min_download:
-        playsound('./sound.mp3')
-        show_toast('Velocidade de download está abaixo do esperado.')
-    elif upload_speed < min_upload:
-        playsound('./sound.mp3')
-        show_toast('Velocidade de upload está abaixo do esperado.')
-    else:
-        print('Está tudo OK.')
-
+def saveToCSV(download_speed, upload_speed, ping, date):
     df = pd.DataFrame({'DOWNLOAD_SPEED': [f'{download_speed:.2f}Mbps'], 'UPLOAD_SPEED': [
                       f'{upload_speed:.2f}Mbps'], 'PING': [f'{ping:.2f}ms'], 'DATE': [date]})
 
     df.to_csv('report.csv', mode='a', index=False, header=False)
+
+
+try:
+    validate()
+    out, err, errcode = speedtest()
+
+    min_download = float(sys.argv[1])
+    min_upload = float(sys.argv[2])
+
+    speed = json.loads(out)  # Transformar em JSON
+
+    # Cálculo para transformar em Mbps
+    download_speed = speed['download']['bandwidth'] * 8 / 1_000_000
+    upload_speed = speed['upload']['bandwidth'] * 8 / 1_000_000
+    ping = speed['ping']['latency']
+    date = ISOtoHuman(speed['timestamp'])
+
+    if download_speed < min_download and upload_speed < min_upload:
+        notify('Velocidade de download e upload estão abaixo do esperado.')
+    elif download_speed < min_download:
+        notify('Velocidade de download está abaixo do esperado.')
+    elif upload_speed < min_upload:
+        notify('Velocidade de upload está abaixo do esperado.')
+    else:
+        print('Está tudo OK.')
+
+    saveToCSV(download_speed, upload_speed, ping, date)
 except MissingArgs:
     print('Necessário mínimo de internet de download e upload aceitável.')
