d23b8cd42905e0c98c8089824b33794797500bdc
Etcetera <pietroricardocres@proton.me>
2023-05-23 09:30:28 -0300

    display

---
+++ display.py
@@ -0,0 +1,57 @@
+import matplotlib.pyplot as plt
+from matplotlib import animation
+import json
+
+
+def read_data_from_json(filename):
+    x = []
+    y1 = []
+    y2 = []
+    y3 = []
+
+    with open(filename, 'r') as file:
+        data = json.loads(file.read())
+
+    for d in data:
+        y1.append(d['download_speed'])
+        y2.append(d['upload_speed'])
+        y3.append(d['ping'])
+        x.append(f"{d['hour']}")
+
+    return x, y1, y2, y3
+
+
+def update_graph(frame):
+    # Atualizar dados a partir do arquivo
+    x, y1, y2, y3 = read_data_from_json('report.json')
+
+    # Limpar os gráficos existentes
+    for ax in axes.flatten():
+        ax.cla()
+
+    # Plotar os novos dados
+    axes[0].plot(x, y1)
+    axes[0].set_title('Download')
+    axes[1].plot(x, y2)
+    axes[1].set_title('Upload')
+    axes[2].plot(x, y3)
+    axes[2].set_title('Ping')
+
+    for ax in axes.flatten():
+        ax.set_xticklabels(ax.get_xticklabels(), rotation=45)
+
+    for ax in axes:
+        ax.spines['top'].set_visible(False)
+        ax.spines['right'].set_visible(False)
+
+    # Ajustar o tamanho do gráfico
+    fig.tight_layout()
+
+
+fig, axes = plt.subplots(nrows=3, ncols=1, figsize=(8, 6))
+
+ani = animation.FuncAnimation(fig, update_graph, interval=1000)
+
+plt.subplots_adjust(hspace=2.0)
+
+plt.show()
